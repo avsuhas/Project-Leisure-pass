7d3986a63ceb9d027a14af733ec7305a57540d75
Suhas V Arudi <avsuhas@gmail.com>
2020-02-28 01:19:09 -0500

    Update LeisureGroupApplication.java

--- leisure-Group/src/main/java/com/lpg/LeisureGroupApplication.java
+++ leisure-Group/src/main/java/com/lpg/LeisureGroupApplication.java
@@ -5,13 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.lpg.repo.CustomerRepo;
-
 @SpringBootApplication
 public class LeisureGroupApplication implements CommandLineRunner{
 	
-	@Autowired
-	CustomerRepo repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeisureGroupApplication.class, args);
